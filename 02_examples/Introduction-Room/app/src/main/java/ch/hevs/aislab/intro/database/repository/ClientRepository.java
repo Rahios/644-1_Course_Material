6857f76213299b9309782b790c3b56ff4ec4fc2c
Elias <55601996+Rahios@users.noreply.github.com>
2022-10-07 09:36:41 +0200

    Update ClientRepository.java

--- 02_examples/Introduction-Room/app/src/main/java/ch/hevs/aislab/intro/database/repository/ClientRepository.java
+++ 02_examples/Introduction-Room/app/src/main/java/ch/hevs/aislab/intro/database/repository/ClientRepository.java
@@ -12,16 +12,21 @@ import ch.hevs.aislab.intro.database.async.UpdateClient;
 import ch.hevs.aislab.intro.database.entity.ClientEntity;
 import ch.hevs.aislab.intro.util.OnAsyncEventListener;
 
-public class ClientRepository {
+public class ClientRepository 
+{
 
     private static ClientRepository instance;
 
     private ClientRepository() {}
 
-    public static ClientRepository getInstance() {
+    // SINGLETON
+    public static ClientRepository getInstance() 
+    {
         if (instance == null) {
-            synchronized (ClientRepository.class) {
-                if (instance == null) {
+            synchronized (ClientRepository.class) 
+            {
+                if (instance == null) 
+                {
                     instance = new ClientRepository();
                 }
             }
@@ -29,23 +34,28 @@ public class ClientRepository {
         return instance;
     }
 
-    public LiveData<ClientEntity> getClient(final String email, Context context) {
+    public LiveData<ClientEntity> getClient(final String email, Context context) 
+    {
         return AppDatabase.getInstance(context).clientDao().getByEmail(email);
     }
 
-    public LiveData<List<ClientEntity>> getAllClients(Context context) {
+    public LiveData<List<ClientEntity>> getAllClients(Context context) 
+    {
         return AppDatabase.getInstance(context).clientDao().getAll();
     }
 
-    public void insert(final ClientEntity client, OnAsyncEventListener callback, Context context) {
+    public void insert(final ClientEntity client, OnAsyncEventListener callback, Context context) 
+    {
         new CreateClient(context, callback).execute(client);
     }
 
-    public void update(final ClientEntity client, OnAsyncEventListener callback, Context context) {
+    public void update(final ClientEntity client, OnAsyncEventListener callback, Context context) 
+    {
         new UpdateClient(context, callback).execute(client);
     }
 
-    public void delete(final ClientEntity client, OnAsyncEventListener callback, Context context) {
+    public void delete(final ClientEntity client, OnAsyncEventListener callback, Context context) 
+    {
         new DeleteClient(context, callback).execute(client);
     }
 }
